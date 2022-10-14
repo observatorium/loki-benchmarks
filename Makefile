dce62a13767ad302711b24008c0a0b3b62891afd
Gerard Vanloo <gvanloo@redhat.com>
2022-10-14 16:31:27 -0400

    Refactor deployment methods

--- Makefile
+++ Makefile
@@ -1,26 +1,22 @@
-export GOBIN=$(CURDIR)/bin
-export PATH:=$(GOBIN):$(PATH)
-
 include .bingo/Variables.mk
 
-export GOROOT=$(shell go env GOROOT)
-export GO111MODULE=on
-
-export REPORT_DIR?=$(CURDIR)/reports/$(shell date +%Y-%m-%d-%H-%M-%S)
-
-export KUBECTL=$(shell which kubectl)
-export USERNAME=$(USER)
+.DEFAULT_GOAL := help
 
 CADVISOR_NAMESPACE := cadvisor
 LOKI_NAMESPACE := observatorium-logs-test
 
-LOKI_STORAGE_BUCKET ?= loki-benchmark-$(USERNAME)
-LOKI_TEMPLATE_FILE ?= /tmp/observatorium-logs-template.yaml
+LOKI_OPERATOR_REGISTRY ?= openshift-logging
+LOKI_STORAGE_BUCKET ?= loki-benchmark-storage
+
 LOKI_CONFIG_FILE ?= config/loki-parameters.yaml
+LOKI_TEMPLATE_FILE ?= /tmp/observatorium-logs-template.yaml
+RHOBS_DEPLOYMENT_FILE ?= /tmp/rhobs-loki-deployment.yaml
 
-OCP_PROM_CONFIG_FILE := /tmp/cluster-monitoring-config.yaml
-
-.DEFAULT_GOAL := help
+ifeq (,$(shell go env GOBIN))
+GOBIN=$(shell go env GOPATH)/bin
+else
+GOBIN=$(shell go env GOBIN)
+endif
 
 ##@ General
 
@@ -35,106 +31,59 @@ OCP_PROM_CONFIG_FILE := /tmp/cluster-monitoring-config.yaml
 # More info on the awk command:
 # http://linuxcommand.org/lc3_adv_awk.php
 
+$(REPORT_DIR):
+	@mkdir -p $(CURDIR)/reports/$(shell date +%Y-%m-%d-%H-%M-%S)
+
 help: ## Display this help.
 	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make \033[36m<target>\033[0m\n"} /^[a-zA-Z_0-9-]+:.*?##/ { printf "  \033[36m%-20s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
 
-##@ Ingredients
+##@ Development
+
 lint: $(GOLANGCI_LINT) ## Lint the code
 	@$(GOLANGCI_LINT) run --timeout=4m
 
-$(REPORT_DIR):
-	@mkdir -p $(REPORT_DIR)
-	@cp reports/README.template $(REPORT_DIR)
-	@mv $(REPORT_DIR)/README.template $(REPORT_DIR)/README.md
-
-download-obs-loki-template: ## Download loki observatorium template locally
+create-rhobs-loki-file: ## Create a yaml file with deployment details for Loki using RHOBS configuration
 	wget -nv -O $(LOKI_TEMPLATE_FILE) https://raw.githubusercontent.com/rhobs/configuration/main/resources/services/observatorium-logs-template.yaml
-.PHONY: download-obs-loki-template
+	oc process -f $(LOKI_TEMPLATE_FILE) -p NAMESPACE=$(LOKI_NAMESPACE) -p LOKI_S3_SECRET=test --param-file $(LOKI_CONFIG_FILE) >> $(RHOBS_DEPLOYMENT_FILE)
+	rm $(LOKI_TEMPLATE_FILE)
+.PHONY:create-rhobs-loki-file
 
-obs-loki-template-cleanup: ## Cleanup local loki observatorium template
-	rm -f $(LOKI_TEMPLATE_FILE)
-.PHONY: obs-loki-template-cleanup
-
-ifeq ($(shell test -e $(LOKI_TEMPLATE_FILE) && echo -n yes),yes)
-deploy-obs-loki: ## Deploy loki from local observatorium template
-	oc create namespace $(LOKI_NAMESPACE)
-	hack/deploy-example-secret.sh $(LOKI_NAMESPACE) $(LOKI_STORAGE_BUCKET)
-	oc process -f $(LOKI_TEMPLATE_FILE) -p NAMESPACE=$(LOKI_NAMESPACE) -p LOKI_S3_SECRET=test --param-file $(LOKI_CONFIG_FILE) | oc -n $(LOKI_NAMESPACE) apply -f -
-else
-deploy-obs-loki:
-	$(error Can't find Loki observatorium deployment template file: $(LOKI_TEMPLATE_FILE). Use "make download-obs-loki-template" to download )
-endif
-.PHONY:deploy-obs-loki
-
-obs-loki-cleanup: ## Cleanup loki deployment
-	oc --ignore-not-found=true delete namespace $(LOKI_NAMESPACE)
-.PHONY: obs-loki-cleanup
-
-##@ Development
-deploy-cadvisor: $(KUSTOMIZE) ## Deploy cadvisor (https://github.com/google/cadvisor)
-	oc create namespace $(CADVISOR_NAMESPACE)
-	$(KUSTOMIZE) build ../cadvisor/deploy/kubernetes/base | oc -n $(CADVISOR_NAMESPACE) apply -f -
-	oc -n $(CADVISOR_NAMESPACE) adm policy add-scc-to-user privileged -z $(CADVISOR_NAMESPACE)
-	oc -n $(CADVISOR_NAMESPACE) adm policy add-cluster-role-to-user cluster-reader -z $(CADVISOR_NAMESPACE)
-.PHONY: deploy-cadvisor
-
-cadvisor-cleanup: ## Cleanup cadvisor
-	oc --ignore-not-found=true delete namespace $(CADVISOR_NAMESPACE)
-.PHONY: cadvisor-cleanup
-
-bench-dev: $(GINKGO) $(PROMETHEUS) $(EMBEDMD) $(REPORT_DIR) ## Execute benchmark
-	@TARGET_ENV=development \
-	KUBECTL=../observatorium/kubectl \
-	OBS_NS=observatorium \
-	OBS_LOKI_QF="observatorium-xyz-loki-query-frontend" \
-	OBS_LOKI_QR="observatorium-xyz-loki-querier" \
-	OBS_LOKI_DST="observatorium-xyz-loki-distributor" \
-	OBS_LOKI_ING="observatorium-xyz-loki-ingester" \
-	./run.sh
-.PHONY: bench-dev
-
-##@ Benchmark Loki on OpenShift
 deploy-s3-bucket: ## Deploy s3 bucket
 	hack/create-s3-bucket.sh $(LOKI_STORAGE_BUCKET)
 .PHONY: deploy-s3-bucket
 
-s3-bucket-cleanup: ## Delete s3 bucket
+s3-bucket-cleanup: ## Destroy s3 bucket
 	hack/delete-s3-bucket.sh $(LOKI_STORAGE_BUCKET)
 .PHONY: s3-bucket-cleanup
 
-deploy-ocp-prometheus: ## Deploy prometheus
-	oc -n openshift-monitoring apply -f hack/ocp-monitoring/cluster-monitoring-config.yaml
-	oc -n openshift-user-workload-monitoring apply -f hack/ocp-monitoring/user-workload-monitoring-config.yaml
-.PHONY: deploy-ocp-prometheus
+deploy-cadvisor: $(KUSTOMIZE) ## Deploy cadvisor
+	kubectl create namespace $(CADVISOR_NAMESPACE)
+	$(KUSTOMIZE) build ../cadvisor/deploy/kubernetes/base | kubectl -n $(CADVISOR_NAMESPACE) apply -f -
+.PHONY: deploy-cadvisor
 
-ocp-prometheus-cleanup: ## Cleanup prometheus deployment
-	oc --ignore-not-found=true -n openshift-monitoring delete -f hack/ocp-monitoring/cluster-monitoring-config.yaml
-	oc --ignore-not-found=true -n openshift-user-workload-monitoring delete  -f hack/ocp-monitoring/user-workload-monitoring-config.yaml
-.PHONY: ocp-prometheus-cleanup
+cadvisor-cleanup: ## Cleanup cadvisor
+	kubectl --ignore-not-found=true delete namespace $(CADVISOR_NAMESPACE)
+.PHONY: cadvisor-cleanup
 
-deploy-ocp-loki: deploy-obs-loki ## Deploy loki
-.PHONY: deploy-ocp-loki
+##@ Testing
 
-ocp-loki-cleanup: obs-loki-cleanup ## Cleanup loki deployment
-.PHONY: ocp-loki-cleanup
+test-benchmarks: $(GINKGO) $(PROMETHEUS) $(EMBEDMD) $(KIND) ## Run benchmark on a Kind cluster
+	@IS_TESTING=true \
+	./run.sh deploy_loki_with_observatorium_configuration $(REPORT_DIR)
+.PHONY: test-benchmarks
 
-ocp-run-benchmarks: $(GINKGO) $(PROMETHEUS) $(EMBEDMD) $(REPORT_DIR) ## Run benchmarks
-	@TARGET_ENV=ocp-observatorium-test \
-	DEPLOY_OCP_PROMETHEUS=true \
-	OBS_NS=$(LOKI_NAMESPACE) \
-	OBS_LOKI_QF="observatorium-loki-query-frontend" \
-	OBS_LOKI_QR="observatorium-loki-querier" \
-	OBS_LOKI_DST="observatorium-loki-distributor" \
-	OBS_LOKI_ING="observatorium-loki-ingester" \
-	./run.sh
-.PHONY: ocp-run-benchmarks
+##@ Deployment
 
-ocp-all-cleanup: obs-loki-template-cleanup s3-bucket-cleanup ocp-prometheus-cleanup  ocp-loki-cleanup ## Cleanup all
-.PHONY: ocp-all-cleanup
+run-rhobs-benchmarks: $(GINKGO) $(PROMETHEUS) $(EMBEDMD) ## Run benchmark on an OpenShift cluster with RHOBS settings
+	@IS_OPENSHIFT=true \
+	BENCHMARK_NAMESPACE=$(LOKI_NAMESPACE) \
+	LOKI_COMPONENT_PREFIX="observatorium-loki" \
+	./run.sh deploy_loki_with_rhobs_configuration $(REPORT_DIR) $(RHOBS_DEPLOYMENT_FILE) $(LOKI_STORAGE_BUCKET)
+.PHONY: run-benchmarks
 
-##@ All-in-one
-ocp-init-bench: ocp-all-cleanup download-obs-loki-template deploy-s3-bucket ## Setup benchmarks on OpenShift (first deployment only)
-.PHONY: ocp-init-bench
-
-ocp-loki-bench: deploy-s3-bucket ocp-prometheus-cleanup deploy-ocp-prometheus ocp-loki-cleanup deploy-ocp-loki ocp-run-benchmarks ## Deploy and Execute loki benchmarks on OCP
-.PHONY: ocp-loki-bench
+run-operator-benchmarks: $(GINKGO) $(PROMETHEUS) $(EMBEDMD)  ## Run benchmark on an OpenShift cluster with Loki Operator
+	@IS_OPENSHIFT=true \
+	BENCHMARK_NAMESPACE="openshift-logging" \
+	LOKI_COMPONENT_PREFIX="lokistack-dev" \
+	./run.sh deploy_loki_with_rhobs_configuration $(REPORT_DIR) $(LOKI_OPERATOR_REGISTRY) $(LOKI_STORAGE_BUCKET)
+.PHONY: run-benchmarks
--- run.sh
+++ run.sh
@@ -2,39 +2,192 @@
 
 set -eou pipefail
 
-TARGET_ENV="${TARGET_ENV:-development}"
-DEPLOY_KIND_OBSERVATORIUM="${DEPLOY_KIND_OBSERVATORIUM:-true}"
-DEPLOY_OCP_PROMETHEUS="${DEPLOY_OCP_PROMETHEUS:-false}"
+source .bingo/variables.env
 
-OBS_NS="${OBS_NS:-observatorium}"
-CADVISOR_NS="${CADVISOR_NS:-cadvisor}"
-OBS_LOKI_QF="${OBS_LOKI_QF:-observatorium-xyz-loki-query-frontend}"
-OBS_LOKI_QR="${OBS_LOKI_QR:-observatorium-xyz-loki-querier}"
-OBS_LOKI_DST="${OBS_LOKI_DST:-observatorium-xyz-loki-distributor}"
-OBS_LOKI_ING="${OBS_LOKI_ING:-observatorium-xyz-loki-ingester}"
+IS_TESTING="${IS_TESTING:-false}"
+IS_OPENSHIFT="${IS_OPENSHIFT:-false}"
+
+CADVISOR_NAMESPACE="${CADVISOR_NAMESPACE:-cadvisor}"
+BENCHMARK_NAMESPACE="${BENCHMARK_NAMESPACE:-observatorium}"
+LOKI_COMPONENT_PREFIX="${$LOKI_COMPONENT_PREFIX:-observatorium-xyz-loki}"
+
+ocp_prometheus_config_directory=config/ocp-monitoring
+
 port_counter=0
 
-trap 'kill $(jobs -p); tear_down; exit 0' EXIT
+# Deploy Loki with the Observatorium configuration
+# Intended to work on a Kind cluster for quick development
+deploy_loki_with_observatorium_configuration() {
+    REPORT_DIR=$1
 
-tear_down() {
-    if [[ "$TARGET_ENV" = "development" ]] && $DEPLOY_KIND_OBSERVATORIUM; then
-        echo -e "\nUndeploying observatorium dev manifests"
-        undeploy_observatorium
+    create_benchmarking_environment
+
+    pushd ../observatorium || exit 1
+    ./configuration/tests/e2e.sh deploy
+    popd
+
+    wait_for_ready_components
+
+    configure_prometheus
+
+    # Run benchmarks
+    $GINKGO --json-report=report.json -output-dir=$REPORT_DIR ./benchmarks
+    
+    # Clean up
+    destroy_benchmarking_environment
+}
+
+# Deploy Loki with the Red Hat Observability Service configuration
+# Intended to work on a OpenShift cluster for benchmarking
+deploy_loki_with_rhobs_configuration() {
+    REPORT_DIR=$1
+    RHOBS_LOKI_FILE=$2
+    LOKI_STORAGE_BUCKET=$3
+
+    create_benchmarking_environment
+
+    kubectl -n $BENCHMARK_NAMESPACE apply -f $RHOBS_LOKI_FILE
+    ./hack/deploy-example-secret.sh $BENCHMARK_NAMESPACE $LOKI_STORAGE_BUCKET
+
+    wait_for_ready_components
+
+    configure_prometheus
+
+    # Run benchmarks
+    $GINKGO --json-report=report.json -output-dir=$REPORT_DIR ./benchmarks
+
+    # Clean Up
+    destroy_benchmarking_environment
+
+    echo -e "\nRemoving RHOBS configuration file"
+    rm $RHOBS_LOKI_FILE
+}
+
+# Deploy Loki with the Red Hat Loki Operator
+# Intended to work on a OpenShift cluster for benchmarking
+deploy_loki_with_red_hat_operator() {
+    REPORT_DIR=$1
+    LOKI_OPERATOR_REGISTRY=$2
+    LOKI_STORAGE_BUCKET=$3
+
+    # Create namespaces
+    create_benchmarking_environment
+    kubectl create namespace openshift-operators-redhat
+    kubectl ns/$BENCHMARK_NAMESPACE openshift.io/cluster-monitoring=true --overwrite
+
+    # Deploy operator and Lokistack
+    pushd ../loki/operator || exit 1
+    make olm-deploy REGISTRY_ORG=$LOKI_OPERATOR_REGISTRY VERSION=v0.0.1
+    ./hack/deploy-aws-storage-secret.sh $LOKI_STORAGE_BUCKET
+    kubectl -n $BENCHMARK_NAMESPACE apply -f hack/lokistack_gateway_ocp.yaml
+    popd
+
+    wait_for_ready_components
+
+    configure_prometheus
+
+    # Run benchmarks
+    $GINKGO --json-report=report.json -output-dir=$REPORT_DIR ./benchmarks
+
+    # Clean Up
+    kubectl delete namespace openshift-operators-redhat --ignore-not-found=true
+    destroy_benchmarking_environment
+}
+
+create_benchmarking_environment() {
+    echo -e "\nCreating benchmarking environment"
+
+    if $IS_TESTING; then
+        $KIND create cluster
+    fi
+
+    kubectl create namespace $BENCHMARK_NAMESPACE
+}
+
+destroy_benchmarking_environment() {
+    echo -e "\nDestroying benchmarking environment"
+
+    if $IS_TESTING; then
+        $KIND delete cluster
+    else
+        kubectl delete namespace $BENCHMARK_NAMESPACE --ignore-not-found=true
+    fi
+
+    if $IS_OPENSHIFT; then
+        disable_ocp_prometheus_monitoring
     fi
 }
 
-deploy_observatorium() {
-    pushd ../observatorium || exit 1
-    ./configuration/tests/e2e.sh kind
-    ./configuration/tests/e2e.sh deploy
-    popd
+configure_prometheus() {
+    echo -e "\nConfiguring Prometheus"
+
+    if $IS_OPENSHIFT; then
+        enable_ocp_prometheus_monitoring
+        export_ocp_prometheus_settings
+    else
+        echo -e "\nForward ports to loki components"
+        forward_ports
+
+        echo -e "\nSet prometheus relabel regex"
+        set_prometheus_relabel_regex
+
+        echo -e "\nScrape metrics from Loki components"
+        scrape_loki_metrics
+    fi
 }
 
-undeploy_observatorium() {
-    pushd ../observatorium || exit 1
-    echo -e "\nUndeploying observatorium dev manifests"
-    ./kind delete cluster
-    popd
+wait_for_ready_components() {
+    echo -e "\nWaiting for available querier deployment"
+    $KUBECTL -n "$BENCHMARK_NAMESPACE" rollout status "deploy/$LOKI_COMPONENT_PREFIX-querier" --timeout=600s
+
+    echo -e "\nWaiting for available loki query frontend deployment"
+    $KUBECTL -n "$BENCHMARK_NAMESPACE" rollout status "deploy/$LOKI_COMPONENT_PREFIX-query-frontend" --timeout=600s
+
+    echo -e "\nWaiting for available loki distributor deployment"
+    $KUBECTL -n "$BENCHMARK_NAMESPACE" rollout status "deploy/$LOKI_COMPONENT_PREFIX-distributor" --timeout=600s
+
+    echo -e "\nWaiting for available loki ingester statefulset"
+    $KUBECTL -n "$BENCHMARK_NAMESPACE" rollout status "statefulsets/$LOKI_COMPONENT_PREFIX-ingester" --timeout=600s
+
+    echo -e "\nWaiting for available loki index gateway statefulset"
+    $KUBECTL -n "$BENCHMARK_NAMESPACE" rollout status "statefulsets/$LOKI_COMPONENT_PREFIX-index-gateway" --timeout=600s
+
+    echo -e "\nWaiting for available loki compactor statefulset"
+    $KUBECTL -n "$BENCHMARK_NAMESPACE" rollout status "statefulsets/$LOKI_COMPONENT_PREFIX-compactor" --timeout=600s
+}
+
+enable_ocp_prometheus_monitoring() {
+    echo -e "\nAdding user workload monitoring configuration"
+
+    kubectl -n openshift-monitoring apply -f $ocp_prometheus_config_directory/cluster-monitoring-config.yaml
+	kubectl -n openshift-user-workload-monitoring apply -f $ocp_prometheus_config_directory/user-workload-monitoring-config.yaml
+}
+
+disable_ocp_prometheus_monitoring() {
+    echo -e "\nRemoving user workload monitoring configuration"
+
+    kubectl -n openshift-monitoring delete -f $ocp_prometheus_config_directory/cluster-monitoring-config.yaml --ignore-not-found=true
+	kubectl -n openshift-user-workload-monitoring delete  -f $ocp_prometheus_config_directory/user-workload-monitoring-config.yaml --ignore-not-found=true
+}
+
+export_ocp_prometheus_settings() {
+    echo -e "\nRetrieving Prometheus URL and bearer token"
+
+    secret=$(kubectl -n openshift-user-workload-monitoring get secret | grep prometheus-user-workload-token | head -n 1 | awk '{print $1 }')
+    export PROMETHEUS_URL="https://$(kubectl -n openshift-monitoring get route thanos-querier -o json | jq -r '.spec.host')"
+    export PROMETHEUS_TOKEN=$(kubectl -n openshift-user-workload-monitoring get secret $secret -o json | jq -r '.data.token' | base64 -d)
+}
+
+forward_ports() {
+    shopt -s extglob
+
+    cp config/prometheus/config.template config/prometheus/config.yaml
+
+    setup_ports "loki query frontend" app.kubernetes.io/component=query-frontend LOKI_QUERY_FRONTEND_TARGETS 3100 "$BENCHMARK_NAMESPACE"
+    setup_ports "loki distributor" app.kubernetes.io/component=distributor LOKI_DISTRIBUTOR_TARGETS 3100 "$BENCHMARK_NAMESPACE"
+    setup_ports "loki ingester" app.kubernetes.io/component=ingester LOKI_INGESTER_TARGETS 3100 "$BENCHMARK_NAMESPACE"
+    setup_ports "loki querier" app.kubernetes.io/component=querier LOKI_QUERIER_TARGETS 3100 "$BENCHMARK_NAMESPACE"
+    setup_ports "cadvisor ingesters" "" CADVISOR_INGESTERS_TARGETS 8080 "$CADVISOR_NAMESPACE"
 }
 
 setup_ports() {
@@ -43,103 +196,32 @@ setup_ports() {
     prometheus_template_targets=$3
     source_port=$4
     namespace=$5
-    echo -e "\nSetup port-forwards to $setup_name pods in namespace $namespace"
+
     qr_targets=""
-    for name in $($KUBECTL -n "$namespace" get pod -l "$match_label" -o name); do
+
+    echo -e "\nSetup port-forwards to $setup_name pods in namespace $namespace"
+    
+    for name in $(kubectl -n "$namespace" get pod -l "$match_label" -o name); do
         destination_port=$((source_port+port_counter))
         echo -e "\nSetup port-forward $destination_port:$source_port to $setup_name pod: $name"
         (
-            $KUBECTL -n "$namespace" port-forward "$name" $destination_port:"$source_port";
+            kubectl -n "$namespace" port-forward "$name" $destination_port:"$source_port";
         ) &
         qr_targets="$qr_targets'localhost:$destination_port',"
         ((port_counter=port_counter+1))
     done
+    
     sed -i "s/{{$prometheus_template_targets}}/${qr_targets%%+(,)}/i" config/prometheus/config.yaml
 }
 
-wait_for_deployments() {
-    echo -e "\nWaiting for available querier deployment"
-    $KUBECTL -n "$OBS_NS" rollout status "deploy/$OBS_LOKI_QR" --timeout=600s
-    echo -e "\nWaiting for available loki query frontend deployment"
-    $KUBECTL -n "$OBS_NS" rollout status "deploy/$OBS_LOKI_QF" --timeout=600s
-    echo -e "\nWaiting for available loki distributor deployment"
-    $KUBECTL -n "$OBS_NS" rollout status "deploy/$OBS_LOKI_DST" --timeout=600s
-    echo -e "\nWaiting for available loki ingester deployment"
-    $KUBECTL -n "$OBS_NS" rollout status "statefulsets/$OBS_LOKI_ING" --timeout=600s
-}
-
-forward_ports() {
-    shopt -s extglob
-
-    cp config/prometheus/config.template config/prometheus/config.yaml
-
-    setup_ports "loki query frontend" app.kubernetes.io/component=query-frontend LOKI_QUERY_FRONTEND_TARGETS 3100 "$OBS_NS"
-    setup_ports "loki distributor" app.kubernetes.io/component=distributor LOKI_DISTRIBUTOR_TARGETS 3100 "$OBS_NS"
-    setup_ports "loki ingester" app.kubernetes.io/component=ingester LOKI_INGESTER_TARGETS 3100 "$OBS_NS"
-    setup_ports "loki querier" app.kubernetes.io/component=querier LOKI_QUERIER_TARGETS 3100 "$OBS_NS"
-    setup_ports "cadvisor ingesters" "" CADVISOR_INGESTERS_TARGETS 8080 "$CADVISOR_NS"
-}
-
 set_prometheus_relabel_regex() {
-    INGESTERS_REGEX=$($KUBECTL get pods -l "app.kubernetes.io/component=ingester" -n "$OBS_NS" -o jsonpath='{range .items[*]}{".*crio-"}{.status.containerStatuses[?(@.name=="observatorium-loki-ingester")].containerID}{".*|"}{end}' | sed -s 's|cri-o://||g')
+    INGESTERS_REGEX=$($KUBECTL get pods -l "app.kubernetes.io/component=ingester" -n "$BENCHMARK_NAMESPACE" -o jsonpath='{range .items[*]}{".*crio-"}{.status.containerStatuses[?(@.name=="observatorium-loki-ingester")].containerID}{".*|"}{end}' | sed -s 's|cri-o://||g')
     INGESTERS_REGEX=${INGESTERS_REGEX%%+(\|)}
     sed -i "s/{{CADVISOR_INGESTERS_TARGETS_PODS}}/$INGESTERS_REGEX/i" config/prometheus/config.yaml
 }
 
 scrape_loki_metrics() {
-    source .bingo/variables.env
     (
         $PROMETHEUS --log.level=warn --config.file=./config/prometheus/config.yaml --storage.tsdb.path="$(mktemp -d)";
     ) &
 }
-
-generate_report() {
-    source .bingo/variables.env
-
-    sed -i "s/{{TARGET_ENV}}/$TARGET_ENV/i" $REPORT_DIR/README.md
-    $EMBEDMD -w $REPORT_DIR/README.md
-
-    for f in $REPORT_DIR/*.gnuplot; do
-      gnuplot -e "set term png; set output '$f.png'" "$f"
-    done
-
-    cat "$REPORT_DIR/result.md" >> "$REPORT_DIR/README.md"
-    rm "$REPORT_DIR/result.md"
-}
-
-bench() {
-    if [[ "$TARGET_ENV" = "development" ]] && $DEPLOY_KIND_OBSERVATORIUM; then
-        echo "Deploying observatorium dev manifests"
-        deploy_observatorium
-    fi
-
-    wait_for_deployments
-
-    if $DEPLOY_OCP_PROMETHEUS; then
-        secret=$($KUBECTL -n openshift-user-workload-monitoring get secret | grep prometheus-user-workload-token | head -n 1 | awk '{print $1 }')
-        export PROMETHEUS_URL="https://$($KUBECTL -n openshift-monitoring get route thanos-querier -o json | jq -r '.spec.host')"
-        export PROMETHEUS_TOKEN=$($KUBECTL -n openshift-user-workload-monitoring get secret $secret -o json | jq -r '.data.token' | base64 -d)
-    else
-        echo -e "\nForward ports to loki deployments"
-        forward_ports
-
-        echo -e "\nSet prometheus relabel regex"
-        set_prometheus_relabel_regex
-
-        echo -e "\nScrape metrics from Loki deployments"
-        scrape_loki_metrics
-    fi
-
-    export DEPLOY_OCP_PROMETHEUS
-    source .bingo/variables.env
-
-    echo -e "\nRun benchmarks"
-    $GINKGO --json-report=report.json -output-dir=$REPORT_DIR -keep-separate-reports  ./benchmarks ||:
-
-    echo -e "\nGenerate benchmark report"
-    generate_report
-}
-
-bench
-
-exit $?
